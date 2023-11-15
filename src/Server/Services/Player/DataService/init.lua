10417742120555f2f87804dd6c87ab25345af262
Joshua DeBoer <joshuadeboer707@gmail.com>
2023-11-15 15:41:12 -0500

    Deprecated the old data service

--- src/Server/Services/Player/DataService/init.lua
+++ src/Server/Services/Player/DataService/init.lua
@@ -1,244 +1,193 @@
--- DataService
--- Author(s): serverOptimist
--- Date: 03/28/2022
+--[=[
+@class DataService
 
---[[
-    
-]]
+Author: serverOptimist & ArtemisTheDeer
+Date: 11/15/2023
+Project: roblox-dsac-boilerplate
 
----------------------------------------------------------------------
+Description: Rewrite of serverOptimist DataService module
+]=]
 
--- Constants
--- local STORE_NAME = ( game:GetService("RunService"):IsStudio() and ("Data_" .. os.time()) ) or "DevelopmentData3"
-local STORE_NAME = "Data_" .. os.time()
-local DATA_KEY = "Player_%s2"
-
--- Knit
-local Packages = game.ReplicatedStorage.Packages
-local Knit = require( Packages:WaitForChild("Knit") )
-local Signal = require(Packages.Signal)
-local Promise = require( Packages.Promise )
-local t = require( Packages.t )
-local Timer = require( Packages.Timer )
-
--- Modules
-
--- Roblox Services
+--GetService calls
 local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
+local ServerScriptService = game:GetService("ServerScriptService")
+local ServerStorage = game:GetService("ServerStorage")
 
--- Variables
-local DataTemplate = require(script.DataTemplate)
-local ProfileService = require( script.ProfileService )
-local ProfileStore = ProfileService.GetProfileStore( STORE_NAME, DataTemplate )
--- Objects
+--Module imports (Require)
+local Knit: table = require(ReplicatedStorage.Packages.Knit)
+local Signal: table = require(ReplicatedStorage.Packages.Signal)
+local Promise: table = require(ReplicatedStorage.Packages.Promise)
+local ProfileService: table = require(script.ProfileService)
 
----------------------------------------------------------------------
+local DataService: table = Knit.CreateService({
+	Name = "DataService",
+	Client = {},
+	_playerdata = {},
+	_playerdataLoaded = Signal.new("PlayerdataLoaded"),
+	_playerdataUnloaded = Signal.new("PlayerdataUnloaded"),
+})
 
+local PACKAGES: Folder = ReplicatedStorage.Packages
+local DATA_TEMPLATE: table = require(script.DataTemplate)
 
--- Creates DataService
-local DataService = Knit.CreateService {
-    Name = "DataService";
-    Client = {
-        ReplicateData = Knit.CreateSignal();
-        ReplicateTableIndex = Knit.CreateSignal();
-    };
-    PlayerData = {};
-    PlayerDataLoaded = Signal.new();
-}
+local IS_STUDIO: boolean = RunService:IsStudio()
 
-local signals: table = {}
+--[=[
+    @prop STORE_NAME string
+    @within DataService
+    The datastore to use with profileservice for storing playerdata
+]=]
+local STORE_NAME: string = "Playerdata"
 
+--[=[
+    @prop DATA_PREFIX string
+    @within DataService
+    The prefix to amend to the key used for saving playerdata (Eg. "Playerdata_123")
+]=]
+local DATA_PREFIX: string = "playerdata_"
 
--- Gets player signal
-local function GetPlayerSignal( player, signalName ): ( {} )
-    local playerSignals = if signals[player] then signals[player] else {}
-    signals[player] = playerSignals
-    local signal = if playerSignals[signalName] then playerSignals[signalName] else Signal.new()
-    playerSignals[signalName] = signal
-    return signal
+--[=[
+    @prop DATA_LOAD_RETRIES number
+    @within DataService
+    The maximum amount of times to try to load a player's data (On joining the game) before rejecting the promise associated w/it
+]=]
+local DATA_LOAD_RETRIES: number = 10
+
+--[=[
+    @prop DATA_LOAD_RETRY_DELAY number
+    @within DataService
+    How long to wait between failed attempts with loading a player's data (On joining the game) before retrying
+]=]
+local DATA_LOAD_RETRY_DELAY: number = 10
+
+--[=[
+    @prop LOAD_PLAYERDATA_IN_STUDIO boolean
+    @within DataService
+    Boolean that determines whether player save profiles should be loaded while in a Roblox studio session
+    If true, playerdata will load in studio. If false, playerdata will not be loaded in studio
+]=]
+local LOAD_PLAYERDATA_IN_STUDIO: boolean = true
+
+--Client knit functions/methods
+
+--[=[
+    Returns a promise that resolves with a table of the player's data, and rejects if it cannot be retrieved for some reason
+    If the playerdata is not loaded already, :_createPlayerdataProfile(Player: Player) will be called server-side first
+    @client
+    @return Promise<T> -- A promise that resolves with a table of the player's data if the playerdata exists, and rejects if the playerdata does not exist
+]=]
+function DataService.Client:GetPlayerdata(Player: Player): table
+	return self.Server:GetPlayerdata(Player)
 end
 
+--Server knit functions/methods
 
--- Gets player data
-function DataService.Client:GetPlayerData( player: Player ): ( {}? )
-    local profile: {}? = self.Server:GetPlayerDataAsync( player )
-    return profile and profile.Data
+--[=[
+    Creates a new playerdata template via profileservice/replicaservice for a player
+    @server
+    @private
+    @return Promise<T> -- A promise that resolves w/a copy of the player's data table if loaded successfully, and rejects if unable to load the player's data
+]=]
+function DataService:_createPlayerdataProfile(Player: Player): table
+	return Promise.retry(
+		Promise.new(function(Resolve, Reject)
+			--A randomly generated GUID is used if the player is in studio & LOAD_PLAYERDATA_IN_STUDIO is set to false
+			local useProductionKey: boolean = (not IS_STUDIO or LOAD_PLAYERDATA_IN_STUDIO)
+			local dataKey: string = useProductionKey and DATA_PREFIX .. Player.UserId or HttpService:GenerateGUID()
+
+			local playerProfile = self._profileStore:LoadProfileAsync(dataKey, "ForceLoad")
+
+			if not playerProfile then
+				return Reject(string.format("Could not load player profile %s", dataKey))
+			end
+
+			--Attach user ID to profile, reconcile data, and kick player/erase key from self._playerdata if they join another session
+			playerProfile:AddUserId(Player.UserId)
+			playerProfile:Reconcile()
+			playerProfile:ListenToRelease(function()
+				if not self._playerdata[Player] then
+					return
+				end
+
+				self._playerdata[Player] = nil
+				self._playerdataUnloaded:Fire(Player)
+				Player:Kick("Your data was loaded on another server. Please rejoin in a few minutes.")
+			end)
+
+			--Cleanup player data when player is leaving the game
+			Player.AncestryChanged:Connect(function(_: any, newParent: any)
+				if not newParent then
+					self._playerdata[Player]._profile:Release()
+					self._playerdata[Player] = nil
+					self._playerdataUnloaded:Fire(Player)
+				end
+			end)
+
+			Resolve(playerProfile)
+		end),
+		DATA_LOAD_RETRIES,
+		DATA_LOAD_RETRY_DELAY
+	):andThen(function(playerProfile: table)
+		self._playerdata[Player] = {
+			_profile = playerProfile,
+		}
+
+		self._playerdataLoaded:Fire(Player)
+	end)
 end
 
+--[=[
+    Returns a promise that resolves with a table of the player's data, and rejects if it cannot be retrieved for some reason
+    If the playerdata is not loaded already, :_createPlayerdataProfile(Player: Player) will be called first
+    @server
+    @return Promise<T> -- A promise that resolves with a table of the player's data if the playerdata exists, and rejects if the playerdata does not exist
+]=]
+function DataService:GetPlayerdata(Player: Player): table
+	return Promise.new(function(Resolve, Reject)
+		if self._playerdata[Player] and self._playerdata[Player]._profile then
+			return Resolve(self._playerdata[Player]._profile.Data)
+		end
 
--- Replicates player data by data with the data to replicate (not recommended to use)
-local tReplicatePlayerData = t.tuple( t.instanceIsA("Player"), t.string, t.optional(t.any) )
-function DataService:ReplicatePlayerData( player: Player, name: string, data: any? ): ()
-    assert( tReplicatePlayerData(player, name, data) )
-
-    self.Client.ReplicateData:Fire( player, name, data )
+		if not self._playerdata[Player] then
+			--If playerdata is not loaded, create new promise & set the _playerdata[Player] key to the new table once promise is resolved
+			self._playerdata[Player] = {
+				_profilePromise = self:_createPlayerdataProfile(Player)
+					:andThen(function(playerProfile: table)
+						Resolve(playerProfile.Data)
+					end)
+					:catch(Reject),
+			}
+		else
+			--If playerdata is being loaded, wait for the _profilePromise to resolve/reject, and act accordingly
+			self._playerdata[Player]._profilePromise
+				:andThen(function(playerProfile: table)
+					Resolve(playerProfile.Data)
+				end)
+				:catch(Reject)
+		end
+	end)
 end
 
-
--- Replicates player's table by the index inside the table
--- Use this method to only replicate an index within a table
--- Example: If I had a table called "Trails" and I just changed some data in Trails.Example I would just do
--- DataService:ReplicateTableIndex( player, "Trails", "Example" )
-local tReplicateTableIndex = t.tuple( t.instanceIsA("Player"), t.string, t.any )
-function DataService:ReplicateTableIndex( player: Player, name: string, index: string ): ()
-    assert( tReplicateTableIndex(player, name, index) )
-    local profile: {} = self:GetPlayerDataAsync( player )
-    if ( not profile ) then return end
-
-    local targetTable: {} = profile.Data[ name ]
-    assert( typeof(targetTable) == "table", string.format("ReplicateTableIndex expects second parameter \"name\" to reference a table in player's data, \"%s\" got %s", name, typeof(targetTable)) )
-
-    GetPlayerSignal( player, name ):Fire( profile.Data[name], player )
-
-    self.Client.ReplicateTableIndex:Fire( player, name, index, targetTable[index] )
+--[=[
+    Initialize DataService
+    @server
+    @return nil
+]=]
+function DataService:KnitInit(): nil
+	local useProductionStore: boolean = (not IS_STUDIO or LOAD_PLAYERDATA_IN_STUDIO)
+	--Use a temporary profilestore key if in studio & LOAD_PLAYERDATA_IN_STUDIO is set to false
+	self._profileStore =
+		ProfileService.GetProfileStore(useProductionStore and STORE_NAME or STORE_NAME .. os.time(), DATA_TEMPLATE)
 end
 
-
--- Gets player data asynchronously (when it gets loaded)
-local tGetPlayerDataAsync = t.tuple( t.instanceIsA("Player") )
-function DataService:GetPlayerDataAsync( player: Player ): ( {}? )
-    assert( tGetPlayerDataAsync(player) )
-    local data
-    repeat
-        data = self.PlayerData[ player ]
-    until ( data ) or ( not player:IsDescendantOf(game) ) or ( not task.wait() )
-    return data
-end
-
-
--- Gets the signal of specific data that gets changed
-local tGetDataChangedSignal = t.tuple( t.string, t.instanceIsA("Player") )
-function DataService:GetDataChangedSignal( signalName: string, player: Player ): ( {} )
-    assert( tGetDataChangedSignal(signalName, player) )
-    return GetPlayerSignal( player, signalName )
-end
-
-
--- Gets player data via a promise
-local tGetPlayerDataPromise = t.tuple( t.instanceIsA("Player") )
-function DataService:GetPlayerDataPromise( player: Player ): ( {} )
-    assert( tGetPlayerDataPromise(player) )
-
-    return Promise.new(function( resolve, reject )
-        local data = self:GetPlayerDataAsync( player )
-        if ( data ) then
-            resolve( data )
-        else
-            reject( "Couldn't get Player's data!" )
-        end
-    end)
-end
-
-		
--- Sets player data based on the name
-local tSetPlayerData = t.tuple( t.instanceIsA("Player"), t.string, t.optional(t.any), t.optional(t.boolean) )
-function DataService:SetPlayerData( player: Player, dataName: string, value: any?, replicateToClient: boolean? ): ()
-    assert( tSetPlayerData(player, dataName, value, replicateToClient) )
-
-    replicateToClient = if replicateToClient ~= nil then replicateToClient else true
-
-    local profile: {} = self:GetPlayerDataAsync( player )
-    if ( not profile ) then return end
-
-    local oldValue: any? = profile.Data[ dataName ]
-    profile.Data[ dataName ] = value
-    GetPlayerSignal( player, dataName ):Fire( value, player )
-
-    if ( replicateToClient ) then
-        self:ReplicatePlayerData( player, dataName, value )
-    end
-end
-
-
--- Increments any numerical data the player has by passed value
-local tIncrementPlayerData = t.tuple( t.instanceIsA("Player"), t.string, t.number, t.optional(t.boolean) )
-function DataService:IncrementPlayerData( player: Player, dataName: string, incrementAmount: number, replicateToClient: boolean? ): ()
-    assert( tIncrementPlayerData(player, dataName, incrementAmount, replicateToClient) )
-
-    replicateToClient = if replicateToClient ~= nil then replicateToClient else true
-
-    local profile: {} = self:GetPlayerDataAsync( player )
-    if ( not profile ) then return end
-
-    local oldValue: number = profile.Data[ dataName ]
-    assert( type(oldValue) == "number", string.format("Tried to increment \"%s\" which is a %s", oldValue, type(oldValue)) )
-    local newValue: number = profile.Data[ dataName ] + incrementAmount
-    profile.Data[ dataName ] = newValue
-
-    GetPlayerSignal( player, dataName ):Fire( newValue, player )
-
-    if ( replicateToClient ) then
-        self:ReplicatePlayerData( player, dataName, newValue )
-    end
-end
-
-				
--- Starts this service
-function DataService:KnitStart(): ()
-
-    local function OnPlayerAdded( player: Player): ()
-        local dataKey = ( RunService:IsStudio() and HttpService:GenerateGUID(false) ) or string.format(DATA_KEY, player.UserId)
-        local profile = ProfileStore:LoadProfileAsync( dataKey, "ForceLoad" )
-        if ( profile ) then
-            profile:AddUserId( player.UserId )
-            profile:Reconcile()
-            profile:ListenToRelease(function()
-                self.PlayerData[ player ] = nil
-                player:Kick( "Your data was loaded on another server. Please rejoin in a few minutes." )
-            end)
-
-            if ( player:IsDescendantOf(game) ) then
-                self.PlayerData[ player ] = profile
-                self.PlayerDataLoaded:Fire( player, profile )
-            else
-                profile:Release()
-            end
-        else
-            player:Kick( "We were unable to load your data. Please rejoin in a few minutes.")
-        end
-    end
-    for index: number, player: Player in next, game.Players:GetPlayers() do 
-        task.spawn(OnPlayerAdded, player)
-    end
-    game.Players.PlayerAdded:Connect( OnPlayerAdded )
-
-    local function ClearPlayerSignals( player: Player ): ()
-        local playerSignals: {}? = signals[ player ]
-        if ( playerSignals ) then
-            for _, signal in pairs( playerSignals ) do
-                signal:Destroy()
-            end
-        end
-        signals[ player ] = nil
-    end
-
-    local function OnPlayerRemoving( player: Player ): ()
-        local profile = self.PlayerData[ player ]
-        if ( profile ) then
-            self.PlayerData[ player ] = nil
-            profile:Release()
-        end
-
-        task.spawn( ClearPlayerSignals, player )
-    end
-    game.Players.PlayerRemoving:Connect( OnPlayerRemoving )
-    -- This is purely a lightweight safety catch for edge-case memory leaks
-    local function SweepOldSignals(): ()
-        for player in pairs( signals ) do
-            if ( not player:IsDescendantOf(game.Players) ) then
-                OnPlayerRemoving( player )
-            end
-        end
-    end
-    Timer.Simple( 300, SweepOldSignals )
-end
-
-
--- Sets up this service
-function DataService:KnitInit(): ()
-    
-end
-
+--[=[
+    Start DataService
+    @server
+    @return nil
+]=]
+function DataService:KnitStart(): nil end
 
 return DataService
